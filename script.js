d7c679bf5bdf5701a8659600c296a5afd8c60820
DavCoder22 <davidelpatron22@gmail.com>
2024-02-21 15:07:26 -0500

    version 2.1

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // JavaScript
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var scrollPosition = $(window).scrollTop() + $(window).height();
-        var cardPosition = $('.dentist-card').offset().top;
+    $('#card').click(function () {
+        $('#body').slideToggle();
+    });
 
-        if (scrollPosition > cardPosition) {
-            $('.dentist-card').addClass('card-show');
-        } else {
-            $('.dentist-card').removeClass('card-show');
-        }
+    $('.contact-info p').hover(function () {
+        $(this).css('background-color', '#d4edda').css('color', '#155724'); // Cambia el color de fondo y el color del texto al pasar el cursor
+    }, function () {
+        $(this).css('background-color', 'transparent').css('color', '#007bff'); // Restaura los colores originales al quitar el cursor
     });
 });
