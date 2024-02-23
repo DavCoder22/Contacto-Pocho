65b2c7f7ffec694a79ec01e9efa0b4a1509b270f
DavCoder22 <davidelpatron22@gmail.com>
2024-02-22 23:47:35 -0500

    version 14

--- js/script.js
+++ js/script.js
@@ -8,16 +8,16 @@ $(document).ready(function () {
         $(this).toggleClass('bg-light text-success'); // Alterna las clases de fondo y color de texto al pasar el cursor
     });
 
-    // Efecto de escritura para el nombre
-    var name = $('#name').text();
-    var nameArray = name.split('');
-    var nameIndex = 2;
-    var nameDirection = 1;
-
     setInterval(function () {
+        if (isWaiting) return; // Si está esperando, no hagas nada
+
         if (nameIndex > nameArray.length) {
             nameDirection = -1;
             nameIndex--; // Evita borrar el segundo carácter
+            isWaiting = true; // Comienza a esperar
+            setTimeout(function () {
+                isWaiting = false; // Termina la espera después de 6 segundos
+            }, 6000);
         } else if (nameIndex < 2) { // Detente en el segundo carácter
             nameDirection = 1;
             nameIndex = 2; // Comienza a llenar desde el segundo carácter
