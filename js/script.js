5854f5849d77ea3f0193dfe4b738def378973a4e
DavCoder22 <davidelpatron22@gmail.com>
2024-02-22 22:27:05 -0500

    version 7

--- js/script.js
+++ js/script.js
@@ -7,4 +7,11 @@ $(document).ready(function () {
     $('.contact-info p').hover(function () {
         $(this).toggleClass('bg-light text-success'); // Alterna las clases de fondo y color de texto al pasar el cursor
     });
+
+    // Efecto de escritura para el nombre
+    var name = $('#name').text();
+    setInterval(function () {
+        name = name[name.length - 1] + name.substring(0, name.length - 1);
+        $('#name').text(name);
+    }, 300);
 });
